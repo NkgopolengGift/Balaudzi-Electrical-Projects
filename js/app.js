fa3975e7b324978e41ada717a441784fc3384061
NkgopolengGift <nkgopolenggift@gmail.com>
2024-05-29 16:49:01 +0200

    Hero section is done 	modified:   css/style.css 	modified:   index.html 	new file:   js/app.js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    //hero slider
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        navText: ['PREV','NEXT'],
+        dots:false,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+});
